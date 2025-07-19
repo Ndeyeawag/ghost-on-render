7c8ac80f75e9e4d1fe2af8ef17553264811ba833
Ndeyeawag <ndeyeawagueye614@gmail.com>
2025-07-19 19:51:10 +0000

    Forcer LF sur start.sh via .gitattributes

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-#!/bin/sh# Cr??e le dossier assets dans le volume persistant s'il n'existe pasmkdir -p /var/lib/ghost/content/assets# Copie le fichier de validation s'il n'existe pas encore dans le volume mont??if [ ! -f /var/lib/ghost/content/assets/148d3015c80c1788169ae32950cb594d.txt ]; then  cp /var/lib/ghost/content/assets/148d3015c80c1788169ae32950cb594d.txt /var/lib/ghost/content/assets/148d3015c80c1788169ae32950cb594d.txtfi# D??marre Ghostghost run
+#!/bin/sh
+
+# Crée le dossier assets dans le volume persistant s'il n'existe pas
+mkdir -p /var/lib/ghost/content/assets
+
+# Copie le fichier de validation s'il n'existe pas encore dans le volume monté
+if [ ! -f /var/lib/ghost/content/assets/148d3015c80c1788169ae32950cb594d.txt ]; then
+  cp /var/lib/ghost/content/assets/148d3015c80c1788169ae32950cb594d.txt /var/lib/ghost/content/assets/148d3015c80c1788169ae32950cb594d.txt
+fi
+
+# Démarre Ghost en mode production
+ghost run --production
